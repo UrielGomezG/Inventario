7e6ea345bdaa2b1ed26e234d3adee386754d4872
UrielGomezG <20233tn177@utez.edu.mx>
2025-06-19 20:54:43 -0600

    agregure que muestre los 4 productos con el que falla y corregi por que le puse "buscado" en vez de buscando

--- U2_TI_11_Inventario/src/App.java
+++ U2_TI_11_Inventario/src/App.java
@@ -14,12 +14,33 @@ public class App {
 
         System.out.println("Total de cosas que me encontre por ahi y ahora quiero venderte " + inventario.totalItems());
 
-        Producto buscado = inventario.buscar("404");
-        if (buscado != null) {
-            System.out.println("trakas, si tenemos eso aqui " + buscado.getNombre() + " y tenemos " + buscado.getCantidad());
+        Producto buscando = inventario.buscar("101");
+        if (buscando != null) {
+            System.out.println("trakas, si tenemos eso aqui " + buscando.getNombre() + " y tenemos " + buscando.getCantidad());
         } else {
             System.out.println("ni sabia si estaba eso pero no lo tenemos");
         }       
+
+        Producto buscando2 = inventario.buscar("202");
+        if (buscando2 != null) {
+            System.out.println("trakas, si tenemos eso aqui " + buscando2.getNombre() + " y tenemos " + buscando2.getCantidad());
+        } else {
+            System.out.println("ni sabia si estaba eso pero no lo tenemos");
+        } 
+        
+        Producto buscando3 = inventario.buscar("303");
+        if (buscando3 != null) {
+            System.out.println("trakas, si tenemos eso aqui " + buscando3.getNombre() + " y tenemos " + buscando3.getCantidad());
+        } else {
+            System.out.println("ni sabia si estaba eso pero no lo tenemos");
+        }  
+        
+        Producto buscando4 = inventario.buscar("404");
+        if (buscando4 != null) {
+            System.out.println("trakas, si tenemos eso aqui " + buscando4.getNombre() + " y tenemos " + buscando4.getCantidad());
+        } else {
+            System.out.println("ni sabia si estaba eso pero no lo tenemos");
+        }      
 
         boolean eliminado = inventario.eliminarProducto("202");
         if (eliminado) {
@@ -28,7 +49,7 @@ public class App {
             System.out.println("No se pudo eliminar");
         }
 
-
+        
 
 
     }
