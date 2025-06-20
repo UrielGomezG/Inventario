694a3520660354b09fd3a48b231b4cf08f0d3161
UrielGomezG <20233tn177@utez.edu.mx>
2025-06-19 20:49:00 -0600

    si funciona el buscar, sumar los objetos y eliminar un objeto

--- U2_TI_11_Inventario/src/App.java
+++ U2_TI_11_Inventario/src/App.java
@@ -1,5 +1,35 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Inventario inventario = new Inventario(3);
+
+        Producto productowan = new Producto("101", "Disco lanzable", 2);
+        Producto productotuo = new Producto("202", "Una pc", 5);
+        Producto productotri = new Producto("303", "Una silla", 4);
+        Producto productofour = new Producto("404", "Un router q me encontre por ahi", 3);
+
+        inventario.agregarProducto(productowan);
+        inventario.agregarProducto(productotuo);
+        inventario.agregarProducto(productotri);
+        inventario.agregarProducto(productofour);
+
+        System.out.println("Total de cosas que me encontre por ahi y ahora quiero venderte " + inventario.totalItems());
+
+        Producto buscado = inventario.buscar("404");
+        if (buscado != null) {
+            System.out.println("trakas, si tenemos eso aqui " + buscado.getNombre() + " y tenemos " + buscado.getCantidad());
+        } else {
+            System.out.println("ni sabia si estaba eso pero no lo tenemos");
+        }       
+
+        boolean eliminado = inventario.eliminarProducto("202");
+        if (eliminado) {
+            System.out.println("tenemos error en el sistema, nuestro cuarto producto no se muestra asi que eliminamos el producto 202");
+        } else {
+            System.out.println("No se pudo eliminar");
+        }
+
+
+
+
     }
 }
